eb557d2686688ae7bb75832aadc8abd50dc9929f
uns3cur3d <1988karlsen@proton.me>
2026-07-02 12:41:23 +0200

    fix (format): fixed formatting

--- internal/auth/get_api_key_test.go
+++ internal/auth/get_api_key_test.go
@@ -6,69 +6,68 @@ import (
 )
 
 func TestGetAPIKeyMissingHeader(t *testing.T) {
- testHeader := http.Header{}
- 
- key, err := GetAPIKey(testHeader)
+	testHeader := http.Header{}
 
- if key != "" {
-	 t.Errorf("expected empty key, got %q", key)
- }
+	key, err := GetAPIKey(testHeader)
 
- if err != ErrNoAuthHeaderIncluded {
-	 t.Errorf("expected ErrNoAuthHeaderIncluded, got %v", err)
- }
+	if key != "" {
+		t.Errorf("expected empty key, got %q", key)
+	}
+
+	if err != ErrNoAuthHeaderIncluded {
+		t.Errorf("expected ErrNoAuthHeaderIncluded, got %v", err)
+	}
 }
 
 func TestGetAPIKeyEmptyAuthorization(t *testing.T) {
- testHeader := http.Header{
-	 "Authorization": []string{""},
- }
- 
- key, err := GetAPIKey(testHeader)
+	testHeader := http.Header{
+		"Authorization": []string{""},
+	}
 
- if key != "" {
-	 t.Errorf("expected empty key, got %q", key)
- }
+	key, err := GetAPIKey(testHeader)
 
- if err != ErrNoAuthHeaderIncluded {
-	 t.Errorf("expected ErrNoAuthHeaderIncluded, got %v", err)
- }
+	if key != "" {
+		t.Errorf("expected empty key, got %q", key)
+	}
+
+	if err != ErrNoAuthHeaderIncluded {
+		t.Errorf("expected ErrNoAuthHeaderIncluded, got %v", err)
+	}
 }
 
 func TestGetAPIKeySplitError(t *testing.T) {
- testHeader := http.Header{
-	 "Authorization": []string{"Malformed Key ApiKey  MyAPIK3y"},
- }
+	testHeader := http.Header{
+		"Authorization": []string{"Malformed Key ApiKey  MyAPIK3y"},
+	}
 
- key, err := GetAPIKey(testHeader)
+	key, err := GetAPIKey(testHeader)
 
- if key != "" {
-	 t.Errorf("expected empty key, got %q", key)
- }
+	if key != "" {
+		t.Errorf("expected empty key, got %q", key)
+	}
 
- if err == nil {
-	 t.Errorf("expected error, got nil")
- }
+	if err == nil {
+		t.Errorf("expected error, got nil")
+	}
 
-
- if err.Error() != "malformed authorization header"  {
-	 t.Errorf("expected malformed authorization header, got %q", err.Error())
- } 
+	if err.Error() != "malformed authorization header" {
+		t.Errorf("expected malformed authorization header, got %q", err.Error())
+	}
 
 }
 
 func TestGetAPIKey(t *testing.T) {
- testHeader := http.Header{
-	 "Authorization": []string{"ApiKey MyAPIK3y"},
- }
- 
- key, err := GetAPIKey(testHeader)
+	testHeader := http.Header{
+		"Authorization": []string{"ApiKey MyAPIK3y"},
+	}
 
- if key == "" {
-	 t.Errorf("expected MyAPIK3y APIKey, got %q", key)
- }
- if err != nil {
-	 t.Errorf("expected error to be nil, got %v", err)
- }
+	key, err := GetAPIKey(testHeader)
+
+	if key == "" {
+		t.Errorf("expected MyAPIK3y APIKey, got %q", key)
+	}
+	if err != nil {
+		t.Errorf("expected error to be nil, got %v", err)
+	}
 
 }
