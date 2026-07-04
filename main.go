3245672550a62e896712230e434880553e6aaa3a
uns3cur3d <1988karlsen@proton.me>
2026-07-04 16:25:35 +0200

    Revert "fix (main): had to run go fmt ./.. to fix a formatting problem"
    This reverts commit 5519c517552241409024e1cdd18ef8deb1565d37.

--- main.go
+++ main.go
@@ -24,6 +24,12 @@ type apiConfig struct {
 //go:embed static/*
 var staticFiles embed.FS
 
+// Unused function
+func unused() {
+	//this function does nothing
+	//and is called nowhere
+}
+
 func main() {
 	err := godotenv.Load(".env")
 	if err != nil {
